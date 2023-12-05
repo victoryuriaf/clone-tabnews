b41e519dc808cb47f68fde0d11b291cde8040162
Victor Yuri <victoryuri.af@gmail.com>
2023-12-05 01:19:48 +0000

    modifica a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 function Home() {
-    return <h1>NaY 4ever</h1>
+    return <>
+        <h1>NaY4ever!</h1>
+        <h2>Cuida xuxu, bora estudar pra gente ficar juntinho no home office!</h2>
+    </>
 }
 
 export default Home;
