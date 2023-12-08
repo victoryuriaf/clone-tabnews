c16098e08e94cdb37672216f48aa358a36aec7b7
Victor Yuri <victoryuri.af@gmail.com>
2023-12-08 01:21:57 +0000

    adiciona scripts `link:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home() {
-    return <h1>NaY4ever! Cuida xuxu, bora estudar pra gente ficar juntinho no home office!</h1>
+  return (
+    <h1>
+      NaY4ever! Cuida xuxu, bora estudar pra gente ficar juntinho no home
+      office!
+    </h1>
+  );
 }
 
 export default Home;
