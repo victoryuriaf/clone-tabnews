8ff54c36528554eb3e7992cc124e6554c35740db
Victor Yuri <victoryuri.af@gmail.com>
2023-12-21 11:41:02 +0000

    add `database.js` and `.env` files

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,8 +1,13 @@
 // request é o que vem de fora (requisição)
 // response é o que vou responder de dentro (resposta)
 
-function status(request, response) {
-  response.status(200).json({ chave: "Alunos do curso.dev são pessoas acima da média" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query('SELECT 1 + 1');
+  response
+    .status(200)
+    .json({ chave: "Alunos do curso.dev são pessoas acima da média" });
 }
 
 export default status;
